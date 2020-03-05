6257643350393e5cf76b4072471a268826e9aa2c
Anurag Goel <Anurag.Goel@bupa-intl.com>
2020-03-05 08:18:52 +0000

    updates

--- project/scripts/clusterbuild.sh
+++ project/scripts/clusterbuild.sh
@@ -5,7 +5,7 @@ if [ $RESULT -gt 0 ]
 then
 gcloud auth activate-service-account \
   my-jenkins-gcr-account@endless-bolt-264307.iam.gserviceaccount.com \
-          --key-file=somekey.json --project=endless-bolt-264307
+          --key-file=/project/scripts/somekey.json --project=endless-bolt-264307
 
 gcloud container clusters create democluster1 --zone us-central1-c  --num-nodes=2 --project endless-bolt-264307
 fi
